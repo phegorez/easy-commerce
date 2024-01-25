6099d5933fc94534e8c2eab7968f0d36cf734ea2
phegorez <metha.nompd@gmail.com>
2024-01-25 21:43:54 +0700

    save

--- src/views/user/CheckoutView.vue
+++ src/views/user/CheckoutView.vue
@@ -38,14 +38,24 @@ const payment = () => {
         <div class="flex">
             <section class="flex-auto w-64 bg-base-200 p-8">
                 <label class="form-control w-full" v-for="formInput in FormData">
-                    <div class="label">
-                        <span class="label-text">{{ formInput.name }}</span>
+                    <div v-if="formInput.name != 'Address'">
+                        <div class="label">
+                            <span class="label-text">{{ formInput.name }}</span>
+                        </div>
+                        <input v-model="userFormData[formInput.field]" type="text" :placeholder="formInput.field"
+                            class="input input-bordered w-full" />
                     </div>
-                    <input v-model="userFormData[formInput.field]" type="text" :placeholder="formInput.field"
-                        class="input input-bordered w-full" />
+                    <div v-else>
+                        <div class="label">
+                            <span class="label-text">{{ formInput.name }}</span>
+                        </div>
+                        <textarea v-model="userFormData[formInput.field]" class="textarea textarea-bordered h-24 w-full"
+                            :placeholder="formInput.field"></textarea>
+                    </div>
                 </label>
                 <button @click="payment" class="btn btn-neutral w-full mt-4">Checkout</button>
             </section>
+
             <section class="flex-auto w-32 bg-base-300">
                 Summary
             </section>
