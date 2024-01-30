5730d6e8c5371e79eb747edd4b9b84518bdbfb4d
phegorez <metha.nompd@gmail.com>
2024-01-30 22:36:12 +0700

    Let build admin site

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,25 @@
 import { createRouter, createWebHistory } from "vue-router";
+// User
 import HomeView from "@/views/user/HomeView.vue";
 import SearchView from "@/views/user/SearchView.vue";
 import ProfileView from "@/views/user/ProfileView.vue";
 import CartView from "@/views/user/CartView.vue";
 import SuccessView from "@/views/user/SuccessView.vue";
 import CheckoutView from "@/views/user/CheckoutView.vue";
+// Admin
+import AdminDashboard from "@/views/admin/DashboardView.vue";
+
+import AdminProdutcList from "@/views/admin/product/ListView.vue";
+import AdminProductUpdate from "@/views/admin/product/UpdateView.vue";
+
+import AdminOrderList from "@/views/admin/order/ListView.vue";
+import AdminOrderDetail from "@/views/admin/order/DetailView.vue";
+
+import AdminUserList from "@/views/admin/user/ListView.vue";
+import AdminUserUpdate from "@/views/admin/user/UpdateView.vue";
+
+// Hybrid
+import LoginView from "@/views/LoginView.vue";
 import LogoutView from "@/views/user/LogoutView.vue";
 import Notfound from "@/views/user/NotfoundView.vue";
 
@@ -41,6 +56,48 @@ const router = createRouter({
       name: "success",
       component: SuccessView,
     },
+    // Admin site
+    {
+      path: "/admin/dashboard",
+      name: 'admin-dashboard',
+      component: AdminDashboard
+    },
+    {
+      path: "/admin/products",
+      name: 'admin-products-list',
+      component: AdminProdutcList
+    },
+    {
+      path: "/admin/products/create",
+      name: 'admin-product-create',
+      component: AdminProductUpdate
+    },
+    {
+      path: "/admin/products/update/:id",
+      name: 'admin-product-update',
+      component: AdminProductUpdate
+    },
+    {
+      path: "/admin/users",
+      name: 'admin-users-list',
+      component: AdminUserList
+    },
+    {
+      path: "/admin/users/update/:id",
+      name: 'admin-users-update',
+      component: AdminUserUpdate
+    },
+    {
+      path: "/admin/orders",
+      name: 'admin-orders-list',
+      component: AdminOrderList
+    },
+    {
+      path: "/admin/orders/details/:id",
+      name: 'admin-orders-details',
+      component: AdminOrderDetail
+    },
+    // 
     {
       path: "/logout",
       name: "logout",
@@ -51,6 +108,11 @@ const router = createRouter({
       name: "notfound",
       component: Notfound,
     },
+    {
+      path: '/admin/login',
+      name: 'admin-login',
+      component: LoginView
+    }
   ],
 });
 
---
+++ src/utility/CenterDiv.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="flex h-screen">
+        <div class="m-auto">
+            <div class="border border-base-200 shadow-xl w-96 mx-auto my-4 p-8 flex flex-col items-center">
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+</template>
