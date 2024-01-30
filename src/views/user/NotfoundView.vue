47cb448c045d2d6b4a2e96625b9799d494e68b88
phegorez <metha.nompd@gmail.com>
2024-01-30 02:50:27 +0000

    update notFound Page

--- src/views/user/NotfoundView.vue
+++ src/views/user/NotfoundView.vue
@@ -1,3 +1,19 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
 <template>
-  <div>Not Found</div>
+  <div class="flex h-screen">
+    <div class="m-auto">
+      <div
+        class="border border-base-200 shadow-xl flex flex-col items-center w-96 mx-auto my-4 p-8"
+      >
+        <div class="font-bold text-9xl">404</div>
+        <div class="font-bold text-2xl">PAGE NOT FOUND</div>
+        <RouterLink :to="{ name: 'home' }" class="btn btn-primary mt-4 w-full"
+          >GO TO HOME PAGE</RouterLink
+        >
+      </div>
+    </div>
+  </div>
 </template>
